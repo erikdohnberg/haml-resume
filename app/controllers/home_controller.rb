d3d5161b9167e7b6b1e1f71789efdc1f5f42b4ef
Erik Dohnberg <erikdohnberg@gmail.com>
2013-04-09 08:05:47 -0400

    First commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+
+	def index
+		@name = "Erik Dohnberg"
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HamlResume::Application.config.secret_token = 'e9deb0956d5e6717822afab3349fe7bccb92b051abff5e2202ace6b137e8185d78f17a711450afdd3d124c0ddc68abd9188e500795b98f99edbcda1c3ab45e65'
